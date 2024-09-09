a48f1704445739f97ad7bfa0d050311b7e7ddc2e
Jeremy Thiebaut <jeremy@diggersfactory.com>
2024-09-09 17:51:57 +0200

    Initialisation of i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
+import "../i18n";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Component {...pageProps} />
-    </div>
+    <Provider store={store}>
+      <div className="min-h-screen bg-gray-100">
+        <Component {...pageProps} />
+      </div>
+    </Provider>
   );
 }
